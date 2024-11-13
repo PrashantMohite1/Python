afbc3a506152fea137db23a13e4e0e2e4e03d24b
Prashant Mohite <mohitep2704@gmail.com>
2024-11-13 09:27:08 +0530

    added optimised code of stars.py

--- String/stars.py
+++ String/stars.py
@@ -15,3 +15,12 @@ for i in reversed(range(number+1)):
         print('*',end='')
     print()
   
+# optimise version of printing stars
+
+for i in range(number):
+    print(i * '*', end='')
+    print()
+
+for i in reversed(range(number)):
+    print(i * '*', end='')
+    print()
