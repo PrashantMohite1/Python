496cc58e2cb005b0e8f66b45ad8cd933b52c8bf4
Prashant Mohite <mohitep2704@gmail.com>
2024-11-14 08:34:48 +0530

    added sum-multiples in questions and solutions

---
+++ Question-and-solutions/sum-multiples.py
@@ -0,0 +1,32 @@
+'''
+
+Question:- 
+
+Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
+
+Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
+
+
+Example 1:
+
+Input: n = 7
+Output: 21
+Explanation: Numbers in the range [1, 7] that are divisible by 3, 5, or 7 are 3, 5, 6, 7. The sum of these numbers is 21.
+
+'''
+
+
+n = 7 
+
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        output = 0
+        for i in range(1,n+1):
+            if i % 3 ==0 or i%5==0 or i % 7==0:
+                output += i
+        return output
+
+
+myobject = Solution()
+print(myobject.sumOfMultiples(7))
+
