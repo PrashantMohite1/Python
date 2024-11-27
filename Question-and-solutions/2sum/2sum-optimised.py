52e062e21b72dba776da717d3d838ee5e9aa777c
Prashant Mohite <mohitep2704@gmail.com>
2024-11-27 09:07:34 +0530

    added 2sum

--- Question-and-solutions/2sum/2sum-optimised.py
+++ Question-and-solutions/2sum/2sum-optimised.py
@@ -1,16 +1,13 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
 
+        n = len(nums)
 
-# nums = [2,7,11,15]
-# target = 9
-# n = len(nums)
+        dict1 = {}
+        lst1 = []
 
-# dict1 = {}
-# lst1 = []
-
-# for i in range(n):
-#     result = target - nums[i]
-#     if result in dict1:
-#         return (dict1[result], i)
-#     dict1[nums[i]] = i
-
-
+        for i in range(n):
+            result = target - nums[i]
+            if result in dict1:
+                return (dict1[result], i)
+            dict1[nums[i]] = i
