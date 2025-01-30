3482f67e03cf6a8f16fca64e4f78b55d6b277daa
Prashant Mohite <prashant.mohite@celeritio.com>
2025-01-30 09:05:54 +0530

    added string-reverse.py problem

---
+++ Question-and-solutions/basics-problems/string/string-reverse.py
@@ -0,0 +1,26 @@
+
+"""
+Q
+
+Given a string s, reverse the string without reversing its individual words. Words are separated by spaces.
+
+Note: The string may contain leading or trailing spaces, or multiple spaces between two words. The returned string should only have a single space separating the words, and no extra spaces should be included.
+
+Examples :
+
+Input: s = " i like this program very much "
+Output: "much very program this like i"
+Explanation: After removing extra spaces and reversing the whole string (not individual words), the input string becomes "much very program this like i". 
+
+
+"""
+
+
+
+s = "I like the Program Very Much"
+
+rev = s.split()
+rev2 = " ".join(rev[::-1])
+print(rev2)
+
+
